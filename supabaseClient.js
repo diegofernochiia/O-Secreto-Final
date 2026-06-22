038b2439f0e6b8a9ae2a96576f4cb1c2a739cf63
diegofernochiia <diegofernochi.ia@gmail.com>
2026-06-22 14:57:20 -0300

    s

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,10 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// Link completo do seu novo projeto que você copiou da primeira opção
+// URL do teu novo projeto "O-Secreto-Final"
 const supabaseUrl = 'https://hwbmkoydbkbdjhqdrwyt.supabase.co'; 
 
-// A nova chave publicável que você copiou da segunda opção
+// A nova chave pública gerada para este projeto específico
 const supabaseKey = 'sb_publishable_X0R5B6o-YipVUju_FIytO-UP'; 
 
-// Inicializa o banco de dados globalmente para o jogo rodar
+// Inicializa a ligação global para que o teu index.html consiga aceder
 window.supabaseClient = createClient(supabaseUrl, supabaseKey);

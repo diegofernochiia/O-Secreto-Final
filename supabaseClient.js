4e71e0f3805cf479129e53e0e45fda94f634c57f
diegofernochiia <diegofernochi.ia@gmail.com>
2026-06-22 15:07:13 -0300

    .

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,6 @@
-// Inicializa o cliente usando a biblioteca global que vamos puxar no HTML
+// URL e Chave do seu projeto "O-Secreto-Final"
 const supabaseUrl = 'https://hwbmkoydbkbdjhqdrwyt.supabase.co'; 
 const supabaseKey = 'sb_publishable_X0R5B6o-YipVUju_FIytO-UP'; 
 
+// Inicializa o cliente globalmente usando o objeto 'supabase' injetado pelo CDN do HTML
 window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);

d703959ace5661c069c90228748da92f7a42952e
diegofernochiia <diegofernochi.ia@gmail.com>
2026-06-22 15:03:09 -0300

    s

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,5 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-// URL do teu novo projeto "O-Secreto-Final"
+// Inicializa o cliente usando a biblioteca global que vamos puxar no HTML
 const supabaseUrl = 'https://hwbmkoydbkbdjhqdrwyt.supabase.co'; 
-
-// A nova chave pública gerada para este projeto específico
 const supabaseKey = 'sb_publishable_X0R5B6o-YipVUju_FIytO-UP'; 
 
-// Inicializa a ligação global para que o teu index.html consiga aceder
-window.supabaseClient = createClient(supabaseUrl, supabaseKey);
+window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);

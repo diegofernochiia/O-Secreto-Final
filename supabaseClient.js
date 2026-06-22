489fc7039e2a74c9c905eeff096b421748e07942
diegofernochiia <diegofernochi.ia@gmail.com>
2026-06-22 14:43:43 -0300

    s

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,10 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// Substitua pelo link correto (sem o rest/v1/)
-const supabaseUrl = 'https://mzlqpzvcvrykfjkigli.supabase.co'; 
+// Link completo do seu novo projeto que você copiou da primeira opção
+const supabaseUrl = 'https://hwbmkoydbkbdjhqdrwyt.supabase.co'; 
 
-// Substitua pelo seu código anon key
-const supabaseKey = 'sb_publishable_ncUaaodlL0AuoXFAmturfw_FIytO-UP'; 
+// A nova chave publicável que você copiou da segunda opção
+const supabaseKey = 'sb_publishable_X0R5B6o-YipVUju_FIytO-UP'; 
 
-// Inicializa o cliente do Supabase corretamente
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Inicializa o banco de dados globalmente para o jogo rodar
+window.supabaseClient = createClient(supabaseUrl, supabaseKey);
